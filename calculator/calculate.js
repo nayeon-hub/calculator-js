a87d537318ba541105068c3742bac0dd41005f58
Park Na Yeon <within197@naver.com>
2021-08-18 23:43:14 +0900

    Initial commit

---
+++ calculator/calculate.js
@@ -0,0 +1,21 @@
+const rowOne = document.querySelector(".row-1");
+const 
+const rowTwo = document.querySelector(".row-2");
+const rowThree = document.querySelector(".row-3");
+const rowFour = document.querySelector(".row-4");
+const rowFive = document.querySelector(".row-5");
+const rowSix = documnet.querySelector(".row-6");
+
+const numBtn = {
+  0: rowSix[0],
+  1: rowFive[0],
+  2: rowFive[1],
+  3: rowFive[2],
+  4: rowFour[0],
+  5: rowFour[1],
+  6: rowFour[2],
+  7: rowThree[0],
+  8: rowThree[1],
+  9: rowThree[2],
+  10: rowSix[1],
+};
