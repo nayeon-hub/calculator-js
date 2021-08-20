5fac06fe591b8f8ba3e5005b75c488553e6be7b7
Park Na Yeon <within197@naver.com>
2021-08-20 17:43:08 +0900

    %, +/- 기능 추가

--- calculator/calculate.js
+++ calculator/calculate.js
@@ -22,18 +22,13 @@ const numBtn = {
   7: rowThreeBtns[0],
   8: rowThreeBtns[1],
   9: rowThreeBtns[2],
-};
-const dotBtn = rowSixBtns[1];
-const resetBtn = rowTwoBtns[0];
-const changeBtn = rowTwoBtns[1];
-const perBtn = rowTwoBtns[2];
-
-const divideBtn = rowTwoBtns[3];
-const multiplyBtn = rowThreeBtns[3];
-const minusBtn = rowFourBtns[3];
-const plusBtn = rowFiveBtns[3];
-const calBtn = [plusBtn, minusBtn, multiplyBtn, divideBtn];
-const resultBtn = rowSixBtns[2];
+  10: rowSixBtns[1],
+}; // 10 - 소수점 버튼
+const calBtn = [rowFiveBtns[3], rowFourBtns[3], rowThreeBtns[3], rowTwoBtns[3]]; // + - * ÷
+const resetBtn = rowTwoBtns[0]; // AC
+const changeBtn = rowTwoBtns[1]; // +/-
+const perBtn = rowTwoBtns[2]; // %
+const resultBtn = rowSixBtns[2]; // =
 
 let num = "";
 let startVal = "";
@@ -75,6 +70,8 @@ function actCalculate(curCal, startVal, endVal) {
     val = multiplyFunc(startVal, endVal);
   } else if (curCal === "divide") {
     val = divideFunc(startVal, endVal);
+  } else {
+    val = Number(startVal);
   }
   return val;
 }
@@ -86,31 +83,62 @@ function showNum(e) {
     resetAct();
     check = false;
   }
-
-  num += e.target.innerHTML;
-  calInput.value = num;
+  if (e.target.innerHTML !== ".") {
+    num += e.target.innerHTML;
+    calInput.value = num;
+  } else {
+    if (num === "") {
+      num += "0.";
+      calInput.value = num;
+    } else {
+      num += ".";
+      calInput.value = num;
+    }
+  }
 
   if (curCal === "") {
     startVal = num;
-    console.log("start:", startVal, "end:", endVal, curCal);
+    console.log("num:", num, "a:", startVal, "b:", endVal, curCal, check);
   } else if (curCal !== "") {
     endVal = num;
-    console.log("start:", startVal, "end:", endVal, curCal);
+    console.log("a:", startVal, "b:", endVal, curCal, check);
   }
 }
 
 function init() {
   calInput.value = 0;
-  curCal = "";
+
   Object.values(numBtn).forEach((ele) => {
     ele.addEventListener("click", showNum);
   });
 
+  changeBtn.addEventListener("click", () => {
+    if (num === "") {
+      num = "-";
+      calInput.value = `${num}0`;
+    } else if (num !== "" && num[0] !== "-") {
+      num = `-${num}`;
+      calInput.value = num;
+      startVal = num;
+    } else if (num !== "" && num[0] === "-") {
+      num = `${num.slice(1, -2)}`;
+      calInput.value = num;
+      startVal = num;
+    }
+  });
+
+  perBtn.addEventListener("click", () => {
+    if (num === "") {
+      calInput.value = "0";
+    } else {
+      num = num / 100;
+      calInput.value = num;
+      startVal = num;
+    }
+  });
+
   calBtn.forEach((ele) => {
     ele.addEventListener("click", (e) => {
-      num = "";
-      curCal = e.target.value;
-      console.log("a:", startVal, "b:", endVal, curCal);
       if (endVal !== "" && check === false) {
         startVal = actCalculate(curCal, startVal, endVal);
         calInput.value = startVal;
@@ -118,14 +146,20 @@ function init() {
       } else if (endVal !== "" && check === true) {
         check = false;
       }
+
+      num = "";
+      curCal = e.target.value;
+      console.log("num:", num, "a:", startVal, "b:", endVal, curCal, check);
     });
   });
 
   resultBtn.addEventListener("click", () => {
     startVal = actCalculate(curCal, startVal, endVal);
     calInput.value = startVal;
-    console.log("a:", startVal, "b:", endVal, curCal);
+    console.log("num:", num, "a:", startVal, "b:", endVal, curCal, check);
+
     check = true;
+    num = String(startVal);
   });
   resetBtn.addEventListener("click", resetAct);
 }
