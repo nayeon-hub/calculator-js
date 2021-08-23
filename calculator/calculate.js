fd6fa68b3686626b9132b2899d536b61c2e1f4aa
Park Na Yeon <within197@naver.com>
2021-08-23 23:47:06 +0900

    오류 수정 및 코드 리팩토링

--- calculator/calculate.js
+++ calculator/calculate.js
@@ -36,6 +36,7 @@ let endVal = "";
 let curCal = "";
 let check = false;
 
+// 모든 값을  reset 시키는 함수
 function resetAct() {
   num = "";
   startVal = "";
@@ -44,75 +45,108 @@ function resetAct() {
   calInput.value = "0";
 }
 
+// '='을 눌렀는지 확인하는 함수 : true이면 완전히 계산이 끝났음을 의미한다.
+function checkCheck() {
+  if (check === true) {
+    resetAct();
+    check = false;
+  }
+}
+
+// 첫번째 값과 두번째값, 사칙연산 기호가 들어왔을 때 연산하는 함수
 function actCalculate(curCal, startVal, endVal) {
   let val = "";
   const a = Number(startVal);
   const b = Number(endVal);
-  if (curCal === "plus") {
-    val = a + b;
-  } else if (curCal === "minus") {
-    val = a - b;
-  } else if (curCal === "multiply") {
-    val = a * b;
-  } else if (curCal === "divide") {
-    val = a / b;
-  } else {
+  switch (curCal) {
+    case "plus":
+      val = a + b;
+      break;
+    case "minus":
+      val = a - b;
+      break;
+    case "multiply":
+      val = a * b;
+      break;
+    case "divide":
+      val = a / b;
+      break;
+  }
+  if (curCal === "") {
     val = Number(startVal);
   }
   return val;
 }
 
+// 숫자를 보여주는 함수
+// 1. 연속 소수점을 체크함
+// 2. 처음 0을 누르고 그 다음 오는 숫자가 일반 숫자일 때 처리함
 function showNum(e) {
   calInput.value = "";
-  console.log(check);
-  if (check === true) {
-    resetAct();
-    check = false;
-  }
+  checkCheck();
 
-  if (e.target.innerHTML !== ".") {
-    if (num === "0" && e.target.innerHTML === "0") {
-      num = e.target.innerHTML;
-      console.log(1, num);
-    } else if (num === "-0" && e.target.innerHTML === "0") {
-      num = e.target.innerHTML;
-      console.log(2, num);
-    } else {
-      num += e.target.innerHTML;
-      console.log(3, num);
-    }
-    calInput.value = num;
-  } else {
-    if (num === "") {
-      num += "0.";
-      calInput.value = num;
-    } else {
-      num += ".";
-      calInput.value = num;
-    }
-  }
+  const n = e.target.innerHTML;
+  switch (n) {
+    case ".":
+      if (num.indexOf(".") !== -1) {
+        // 이미 소수점이 있으면 ERROR
+        calInput.value = "ERROR";
+        check = true;
+      } else {
+        if (num === "0" || num === "-0") {
+          // 아무것도 입력되지 않았을 때는 소수점을 누르면 0.
 
-  if (curCal === "") {
-    startVal = num;
-    console.log("num:", num, "a:", startVal, "b:", endVal, curCal, check);
-  } else if (curCal !== "") {
-    endVal = num;
-    console.log("a:", startVal, "b:", endVal, curCal, check);
+          calInput.value = `${num}.`;
+        } else if (num === "") {
+          num = "0.";
+          calInput.value = num;
+        } else {
+          // 소수점이 없으면 PASS
+          num += ".";
+          calInput.value = num;
+        }
+      }
+      break;
+    default:
+      // 일반 숫자 선택했을 경우
+      // 1. 첫번째 숫자가 0일때 처리
+      if (num === "0") {
+        // 0이 먼저 입력되어 있을때
+        num = e.target.innerHTML;
+      } else if (num === "-0") {
+        // -0이 먼저 입력되어 있을 때
+        num = `-${e.target.innerHTML}`;
+      } else {
+        // 그 외에 숫자가 들어왔을 때
+        num += e.target.innerHTML;
+      }
+      calInput.value = num;
+
+      // 2. start값인지 end값인지 판단
+      if (curCal === "") {
+        //사칙연산 하기 전이면 start
+        startVal = num;
+        console.log("num:", num, "a:", startVal, "b:", endVal, curCal, check);
+      } else if (curCal !== "") {
+        //사칙연산 한 후면 end
+        endVal = num;
+        console.log("a:", startVal, "b:", endVal, curCal, check);
+      }
+      break;
   }
 }
 
 function init() {
   calInput.value = 0;
 
+  // 숫자 버튼
   Object.values(numBtn).forEach((ele) => {
     ele.addEventListener("click", showNum);
   });
 
+  // +/- 버튼
   changeBtn.addEventListener("click", () => {
-    if (check === true) {
-      resetAct();
-      check = false;
-    }
+    checkCheck();
 
     if (num === "") {
       num = "-";
@@ -132,6 +166,7 @@ function init() {
     }
   });
 
+  // % 버튼
   perBtn.addEventListener("click", () => {
     if (num === "") {
       calInput.value = "0";
